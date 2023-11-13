ac82ee538e2fb81e2c1a613072d6449b2089bac9
Gabrielle <gabisouza234ra@gmail.com>
2023-11-12 23:48:46 -0300

    Creating the landing page and the store page, improving the CSS and structure of the pages, creating new JS

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,37 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.left-side');
+let menuBtn = document.querySelector('.hamb')
+const menuReduzido = document.querySelector('.menu-reduzido')
+const hambBtn = document.querySelector('.menu-hamb-icon')
+const fecharBtn = document.querySelector(
+  '.menu-x-icon'
+)
+const menuLinks = document.querySelectorAll('.menu-link')
 
-menu.onclick = () => {
-    menu.classList.toggle('fa-x');
-    navbar.classList.toggle('open');
+menuBtn.addEventListener('click', () => {
+  if (menuReduzido.classList.contains('menu-reduzido--active')) {
+    menuReduzido.classList.remove('menu-reduzido--active')
+  } else {
+    menuReduzido.classList.add('menu-reduzido--active')
+  }
+  if (hambBtn.classList.contains('d-none')) {
+    hambBtn.classList.remove('d-none')
+    fecharBtn.classList.add('d-none')
+  } else {
+    hambBtn.classList.add('d-none')
+    fecharBtn.classList.remove('d-none')
+  }
+})
+
+for (let i = 0; i < menuLinks.length; i++) {
+  menuLinks[i].addEventListener('click', () => {
+    menuReduzido.classList.remove('menu-reduzido--active')
+    hambBtn.classList.remove('d-none')
+    fecharBtn.classList.add('d-none')
+  })
 }
 
-let modalidade = document.querySelector('.modalidade');
+// ---
+// const headerLogoConatiner = document.querySelector('.header__logo-container')
 
+// headerLogoConatiner.addEventListener('click', () => {
+//   location.href = 'index.html'
+// })
