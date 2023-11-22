4390a37705f5621e8f78d4b84189a43d64ac6842
Gabrielle <gabisouza234ra@gmail.com>
2023-11-22 03:03:59 -0300

    Improving lugares page and adding javascript

--- js/lugares.js
+++ js/lugares.js
@@ -1,33 +1,60 @@
-		const description = document.querySelector(".tabela");
+document.addEventListener("DOMContentLoaded", function() {
+    const estados = document.querySelectorAll(".estado");
 
-		document.querySelectorAll('path').forEach((el) =>
-			el.addEventListener('mouseover', (event) => {
-				event.target.className = ("enabled");
-				description.classList.add("active");
-				description.innerHTML = event.target.id;
-			})
+    estados.forEach(estado => {
+        estado.addEventListener("click", function() {
+            const estadoId = estado.getAttribute("id");
+            const tabelaCorrespondente = document.getElementById(`tabela${estadoId.charAt(0).toUpperCase() + estadoId.slice(1)}`);
 
-		);
+            if (tabelaCorrespondente) {
+                const tabelas = document.querySelectorAll(".tabela-locais");
+                tabelas.forEach(tabela => tabela.style.display = "none");
 
-		document.querySelectorAll('path').forEach((el) =>
-			el.addEventListener("mouseout", () => {
-				description.classList.remove("active");
-			})
-		);
+                tabelaCorrespondente.style.display = "block";
+            }
+        });
+    });
 
-		document.querySelector('path').forEach((el) =>
-			el.addEventListener('click', (event) => {
-			// event.target.className = ("enabled");
-			// description.classList.add("active");
-			// description.innerHTML = event.target.id;
+    function mostrarMapa(link, mapaID) {
+        const mapa = document.getElementById(mapaID);
+        const iframe = mapa.querySelector('iframe');
+        const url = link.href;
 
-			})
-		);
-		
-	  
+        iframe.src = url;
+        mapa.style.display = 'block';
 
-		document.onmousemove = function (e) {
-			description.style.left = e.pageX + "px";
-			description.style.top = (e.pageY - 70) + "px";
-		}
+        // Esconder outros mapas
+        const outrosMapas = document.querySelectorAll('.mapa');
+        outrosMapas.forEach(outroMapa => {
+            if (outroMapa.id !== mapaID) {
+                outroMapa.style.display = 'none';
+            }
+        });
 
+        // Adicionar evento para ocultar o iframe ao clicar fora dele
+        function esconderIframe(event) {
+            const clicadoForaDaTabela = !event.target.closest('.tabela-locais');
+            const clicadoForaDoIframe = !event.target.closest(`#${mapaID}`);
+
+            if (clicadoForaDaTabela && clicadoForaDoIframe) {
+                iframe.style.display = 'none';
+                document.removeEventListener('click', esconderIframe);
+            }
+        }
+
+        document.removeEventListener('click', esconderIframe);
+        document.addEventListener('click', esconderIframe);
+    }
+
+    // Adicionando eventos de clique aos links de localização
+    const linksLocalizacao = document.querySelectorAll('.tabela-locais table a');
+    linksLocalizacao.forEach((link, index) => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const tabelaPai = this.closest('.tabela-locais'); // Encontrar a tabela correspondente
+            const mapaID = `loc${index + 1}`; // Gerar ID do mapa baseado no índice do link
+
+            mostrarMapa(this, mapaID);
+        });
+    });
+});
